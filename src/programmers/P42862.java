991f6132b128c174b30962db87217a2c0805b19d
JEONG-JIWHAN <jjw981210@gmail.com>
2023-01-17 22:19:36 +0900

    solve: 체육복(프로그래머스 lv1)

---
+++ src/programmers/P42862.java
@@ -0,0 +1,39 @@
+package programmers;
+
+
+import java.util.Arrays;
+
+// greedy 체육복
+public class P42862 {
+
+    static int[] clothes;
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        clothes = new int[n+1];
+        for(int l: lost) {
+            clothes[l]--;
+        }
+        for(int r: reserve) {
+            clothes[r]++;
+        }
+
+        for(int i=1; i<n; i++) {
+            if(clothes[i]==-1 && clothes[i+1]>0) {
+                clothes[i]++;
+                clothes[i+1]--;
+            }
+            if(clothes[i]>0 && clothes[i+1]==-1) {
+                clothes[i+1]++;
+                clothes[i]--;
+            }
+        }
+
+        for(int i=1; i<=n; i++) {
+            if(clothes[i]>-1){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
