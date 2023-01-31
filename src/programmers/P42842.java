b154273c1d96ca5e163136cdc7997fd9ddbcfa72
JEONG-JIWHAN <jjw981210@gmail.com>
2023-01-31 14:44:53 +0900

    solve: 카펫(프로그래머스 lv2)

---
+++ src/programmers/P42842.java
@@ -0,0 +1,16 @@
+package programmers;
+
+// 카펫, 완전탐색 , *
+public class P42842 {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int s = (brown+4)/2;
+        for(int i=3; i<s/2+1; i++) {
+            if((i-2)*(s-i-2) == yellow){
+                answer[0]=s-i;
+                answer[1]=i;
+            }
+        }
+        return answer;
+    }
+}
