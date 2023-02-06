2c145215180c94f1b476dd896348e66a574a308f
JEONG-JIWHAN <jjw981210@gmail.com>
2023-02-06 17:46:44 +0900

    solve: 올바른 괄호(프로그래머스 lv2)

---
+++ src/programmers/P12909.java
@@ -0,0 +1,32 @@
+package programmers;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+// 스택/큐, 올바른 괄호
+public class P12909 {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i=0; i< s.length(); i++) {
+            if(s.charAt(i)=='(') {
+                stack.push(1);
+            }
+            else{
+                if(stack.peek()==null){
+                    answer = false;
+                    return answer;
+                }
+                if(stack.peek()==1){
+                    stack.pop();
+                }
+            }
+        }
+        if(stack.size() != 0) {
+            answer = false;
+        }
+
+        return answer;
+    }
+}
