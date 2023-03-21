67c78b17a792b4bb22842d0afbfe9972010ca41a
JEONG-JIWHAN <jjw981210@gmail.com>
2023-03-21 21:15:30 +0900

    solve: 전화번호 목록(프로그래머스 lv2)

---
+++ src/programmers/P42577.java
@@ -0,0 +1,23 @@
+package programmers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//해시, 전화번호 목록, 고득점 kit
+public class P42577 {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Set<String> stringSet = new HashSet<>();
+        for(String s : phone_book) {
+            stringSet.add(s);
+        }
+        for(String s: phone_book) {
+            for(int i=0; i<s.length(); i++) {
+                if(stringSet.contains(s.substring(0,i))){
+                    answer = false;
+                }
+            }
+        }
+        return answer;
+    }
+}
