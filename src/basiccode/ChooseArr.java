be2d2ab0f11cc7fbfb5e5101715e690ea73bc972
stophwan <jjw981210@gmail.com>
2023-10-06 23:10:43 +0900

    chooseArr

---
+++ src/basiccode/ChooseArr.java
@@ -0,0 +1,41 @@
+package basiccode;
+
+import java.util.Arrays;
+
+public class ChooseArr {
+	static int cnt;
+
+	static void choose1(int cnt, int idx, int[] res) {
+		if(idx==2) {
+			System.out.println(Arrays.toString(res));
+			return;
+		}
+		for(int i=idx; i<res.length; i++) {
+			res[i]++;
+			choose1(cnt+1, i+1, res);
+			res[i]--;
+		}
+	}
+	static void choose2(int idx, int[] res, int[] info) {
+		if(idx==2) {
+			cnt++;
+			System.out.println(Arrays.toString(res));
+			return;
+		}
+		for(int i=0; i<3; i++) {
+			//System.out.println("idx:" + idx+ " i:" + i + " " + Arrays.toString(res));
+			if(res[i]>info[i]) {
+				break;
+			}
+			res[i]++;
+			choose2(idx+1, res, info);
+			res[i]--;
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] info = new int[]{0,1,1};
+		choose1(0, 0, new int[]{0,0,0});
+
+	}
+}
