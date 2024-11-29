eceb8c409d7c8beda8b7da8b7ecf5f728da7399a
stophwan <jjw981210@gmail.com>
2024-11-30 00:07:40 +0900

    package

--- src/baekjoon/mst/BJ4386_별자리.java
+++ src/baekjoon/mst/BJ4386_별자리.java
@@ -1,4 +1,4 @@
-package baekjoon;
+package baekjoon.mst;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
--- src/baekjoon/queue/BJ7662_이중우선순위큐.java
+++ src/baekjoon/queue/BJ7662_이중우선순위큐.java
@@ -1,4 +1,4 @@
-package baekjoon;
+package baekjoon.queue;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
